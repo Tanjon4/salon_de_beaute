ef7468f8e5d074db522ac7dd5c5c646dabc7d5b8
Tanjona <andrianiainatanjona67@gmail.com>
2025-05-20 03:44:01 +0300

    register page

--- home/urls.py
+++ home/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home , name='home'),
-    path('', views.register_page, name='register'),
-    path('',views.login_page, name='login')
+    path('register/', views.register_page, name='register'),
+    path('login/',views.login_page, name='login')
 ]
